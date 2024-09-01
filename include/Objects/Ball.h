40ab3aaaf5d76351eaecc779def40a3d494a7b2e
sahdeek <sahdeek.thompson@gmail.com>
2024-09-01 14:30:56 -0500

    separate ball header to cpp file

--- include/Objects/Ball.h
+++ include/Objects/Ball.h
@@ -6,7 +6,6 @@
 #include <glm/fwd.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
-#include <vector>
 
 #include "Renderer/Shader.h"
 
@@ -21,65 +20,21 @@ class Ball {
     float radius;
     float mass;
 
-    vec2 velocity = vec2(0.0f);
-    float resCoeff = 0.9f;
+    vec2 velocity;
+    float resCoeff;
 
-    vec4 color = vec4(1.0f);
+    vec4 color;
 
-    Ball(vec2 position, float radius, float mass, Shader &shader)
-        : position(position), radius(radius), mass(mass), shader(shader) {
-        initVertexData();
-    }
+    Ball(vec2 position, float radius, float mass, Shader &shader);
 
-    void render() {
-        this->shader.use();
-
-        mat4 model = mat4(1.0f);
-
-        model = translate(model, vec3(this->position, 1.0f));
-        model = scale(model, vec3(vec2(this->radius), 1.0f));
-
-        unsigned int modelLoc = glGetUniformLocation(this->shader.ID, "model");
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, value_ptr(model));
-
-        glBindVertexArray(this->VAO);
-        glDrawArrays(GL_TRIANGLE_FAN, 0, this->segments + 2);
-    }
+    void render();
 
    private:
     Shader shader;
     GLuint VAO;
     GLuint segments = 20;
 
-    void initVertexData() {
-        vector<float> vertices;
-        GLuint VBO;
-
-        vertices.push_back(0.0f);
-        vertices.push_back(0.0f);
-
-        for (int i = 0; i <= segments; i++) {
-            GLfloat angle = 2 * PI * i / segments;
-            GLfloat x = 1.0f * cos(angle);
-            GLfloat y = 1.0f * sin(angle);
-            vertices.push_back(x);
-            vertices.push_back(y);
-        }
-
-        glGenVertexArrays(1, &this->VAO);
-        glGenBuffers(1, &VBO);
-
-        glBindBuffer(GL_ARRAY_BUFFER, VBO);
-        glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(),
-                     GL_STATIC_DRAW);
-
-        glBindVertexArray(this->VAO);
-        glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void *)0);
-        glEnableVertexAttribArray(0);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-    }
+    void initVertexData();
 };
 
 #endif
